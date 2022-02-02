303a9919e8b1202c348da9e3777cc4f3c8e95de9
Augusto Achá <40480509+augusto000@users.noreply.github.com>
2022-02-02 20:59:06 -0300

    Deleted colder.py

--- game/colder.py
+++
@@ -1,8 +0,0 @@
-class Colder:
-    def __init__(self):
-        self._colderNumber=0
-
-    def outPutColder(self):
-        print("(^.^) Getting Colder!")
-#colder = Colder()
-#colder.outPutColder()
